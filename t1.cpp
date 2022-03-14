75e672b6dcaf325941bf50982c8b412324509ae8
OldCadre <101511465+OldCadre@users.noreply.github.com>
2022-03-14 16:43:50 +0800

    Add files via upload

---
+++ t1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+//#include<cmath>
+#include<climits>
+int main()
+{
+	using namespace std;
+	cout.setf(ios_base::fixed, ios_base::floatfield);
+	long long a = 31415555;
+	short b = a;
+	cout << a << endl << b;
+	
+	
+}
